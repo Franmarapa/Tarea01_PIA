8a785fb12e73c07bdecffe63681622aa6ef864a7
Franmarapa <franmarapa@gmail.com>
2025-11-16 17:42:11 +0000

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,5 @@
+hola_mundo.py
+
+Programa básico para imprimir un mensaje en python
+Print es el comando que hace imprimir un texto en el programa, seguido de paréntesis y comillas
+print("Hola Mundo! Estoy dando mis primeros pasos en IA.")
